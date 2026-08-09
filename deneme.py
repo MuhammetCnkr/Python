4cce65f7099a3d0ec545ada135b078ef0d0999fc
MuhammetCnkr <mcinkir.224@gmail.com>
2026-08-09 14:23:35 +0000

    2026-08-09 17:23:35 auto save

--- deneme.py
+++ deneme.py
@@ -1,21 +1,15 @@
-list = []
+# Yöntem 1: Homoglif (Aynı görünen farklı Unicode rakamları)
+def make_homoglyph(phone_str):
+    # Standard 0-9 rakamlarını Mathematical Monospace karşılıklarına eşler
+    mapping = str.maketrans("0123456789", "𝟢𝟣𝟤𝟥𝟦𝟧𝟨𝟩𝟪𝟫")
+    return phone_str.translate(mapping)
 
-while True:
-    try:
-        data = input("Enter: ")
-    except EOFError:
-        break
-    else:
-        list.append(data)
+# Yöntem 2: Zero-Width Space gizleme
+def make_invisible_split(phone_str):
+    zws = "\u200B"  # Görünmez karakter
+    return zws.join(phone_str)
 
+numara = "05368141575"
 
-if len(list) % 2 == 1:
-    uzunluk = (len(list)-1)/2
-else:
-    uzunluk = int(len(list)/2)
-
-for i in range(1,len(list)):
-    tut = list[i-1]
-    list[i-1] = list[-i]
-    list[-1] = tut
-print(list)
+print("Homoglif:", make_homoglyph(numara))
+print("Görünmez karakterli:", make_invisible_split(numara))
